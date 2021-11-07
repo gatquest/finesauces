ee1ee4f936224138438f5ddd735a7d0e818fe621
gatquest <gatquest@gmail.com>
2021-11-07 21:32:31 +0300

    add models, add admin

---
+++ listings/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from .models import Category, Product
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display=('name','slug')
+    prepopulated_fields = {'slug': ('name', )}
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'slug', 'price', 'available')
+    list_filter = ('category', 'available')
+    list_editable =('price', 'available')
+    prepopulated_fields = {'slug': ('name',)}
+# Register your models here.
