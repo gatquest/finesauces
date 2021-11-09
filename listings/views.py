331c14b4e5faed9abffc37a85d5d7eab000f631e
gatquest <gatquest@gmail.com>
2021-11-09 23:27:38 +0300

    added html, css, js for base.html, added urls, views(for base.html

--- listings/views.py
+++ listings/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Category, Product
 
+def product_list(request):
+    categories = Category.objects.all()
+    products = Product.objects.all()
+
+    return render(
+        request,
+        'product/list.html',
+        {
+            'categories': categories,
+            'products': products
+        }
+    )
 # Create your views here.
