ba92c879e92247e0cf460bf5c1d5f9bc19b79a59
gatquest <gatquest@gmail.com>
2021-11-12 00:27:53 +0300

    add list.html for each product

--- listings/models.py
+++ listings/models.py
@@ -36,3 +36,9 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse(
+            'listings:product_detail',
+            args=[self.category.slug, self.slug]
+        )
